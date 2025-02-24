c8e5764597fd5a0dcd48be7af820bdb633c76c0c
Paul Meyers <authenticpaulmeyers@gmail.com>
2025-02-24 13:25:41 +0200

    Forms file configured

---
+++ forms/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import Email, DataRequired, EqualTo
+from wtforms import StringField, EmailField, PasswordField, SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+class LoginForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+class RegisterForm(FlaskForm):
+    firstname = StringField("Firstname", validators=[DataRequired()], render_kw={"placeholder": "Firstname"})
+    lastname = StringField("Lastname", validators=[DataRequired()], render_kw={"placeholder": "Lastname"})
+    email = EmailField("Email", validators=[DataRequired(), Email()], render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField("Password", validators=[DataRequired()], render_kw={"placeholder": "Confirm password"})
+    submit = SubmitField("Register")
+
+class AddProducts(FlaskForm):
+    productName = StringField("Product name", validators=[DataRequired()], render_kw={"placeholder": "Product name"})
+    description = StringField("Product description", validators=[DataRequired()], render_kw={"placeholder": "Product description"})
+    price = StringField("Price", validators=[DataRequired()], render_kw={"placeholder": "Price"})
+    image = FileField("Product image", validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'File format not allowed!')])
+    submit = SubmitField("Add product")
+
+
+
